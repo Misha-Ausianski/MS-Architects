7c8785ac6398cc92d0283c595892b1304e99f54b
Михаил Авсянский <mihail.avsyanskij@infourok.biz>
2025-10-05 12:06:59 +0300

    Updates

---
+++ assets/Button-CUTYaWtI.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C4Fm2UEm.js";const t={};function i({variant:n="primary",className:o="",...s}){return r.jsx("button",{className:[t.btn,t[n],o].join(" "),...s})}export{i as B};
