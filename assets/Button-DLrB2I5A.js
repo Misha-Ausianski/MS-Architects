ee33204ef98d0034b71de1dab2a0952c1a2d7f9d
Михаил Авсянский <mihail.avsyanskij@infourok.biz>
2025-10-10 13:45:26 +0300

    Updates

---
+++ assets/Button-DLrB2I5A.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CM0wMAmI.js";const t={};function i({variant:n="primary",className:o="",...s}){return r.jsx("button",{className:[t.btn,t[n],o].join(" "),...s})}export{i as B};
