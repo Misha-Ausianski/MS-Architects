88b3bf3f529684d10a5b0b07268c36e0903d33e2
Михаил Авсянский <mihail.avsyanskij@infourok.biz>
2025-10-19 17:31:29 +0300

    Updates

--- assets/Button--n7jQFcy.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CDFw7tbj.js";const t={};function i({variant:n="primary",className:o="",...s}){return r.jsx("button",{className:[t.btn,t[n],o].join(" "),...s})}export{i as B};
---
+++ assets/Button-B8Df_Bfr.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Dk01a1Kh.js";const t={};function i({variant:n="primary",className:o="",...s}){return r.jsx("button",{className:[t.btn,t[n],o].join(" "),...s})}export{i as B};
